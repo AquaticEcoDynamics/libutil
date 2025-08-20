264e4d770dcd4806a9f212e6799e2127c675985e
Casper <casper.boon@uwa.edu.au>
2025-08-20 14:59:39 +0800

    updated vs project file

--- src/aed_csv.c
+++ src/aed_csv.c
@@ -66,7 +66,7 @@ static AED_CSV_IN csv_if[MAX_IN_FILES];
 
 static const AED_REAL missing = MISVAL;
 //static const AED_REAL zero = 0.;
-// VS C compiler doesnt like the first for, but is OK with t'other
+// VS C compiler doesnt like the first form, but is OK with t'other
 //static const AED_REAL NaN = missing / zero;
 static const AED_REAL NaN = MISVAL / 0.;
 
